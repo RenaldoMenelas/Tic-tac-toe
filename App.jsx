25285e5e49f469db0c44d3068dc548d3f9e097b0
Renaldo Menelas <renaldomenelas@Renaldos-Air.home>
2023-12-12 19:02:54 -0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import Square from './Square'
+import Board from './Board'
+
+export default function App() {
+  return (
+
+    <>
+      <h1>Tic Tac Toe</h1>
+      <Board/>
+     
+      </>
+   )
+ }
